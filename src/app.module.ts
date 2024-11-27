4076a2809d5864d60cc2fd82aec7ad11b6fd88a3
kimiavedad <kimia.ved@gmail.com>
2024-11-27 14:52:22 +0330

    Add typeorm config to apply migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './products/entities/product.entity';
-import { ProductHistory } from './products/entities/product-history.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from './config/typeorm';
 
 @Module({
   imports: [
     ProductsModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'admin',
-      password: 'password',
-      database: 'online_store',
-      entities: [Product, ProductHistory],
-      synchronize: true,
-      // shouldn't be used in production
-      // TODO: create an env file and set up these items from there
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
     }),
   ],
   controllers: [AppController],
