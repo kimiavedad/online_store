02f527b09a2884cd778945eaa5f0d57256b9c33c
kimiavedad <kimia.ved@gmail.com>
2024-11-29 15:02:38 +0330

    Add display product query and query handler

---
+++ src/products/queries/handlers/display-product.handler.ts
@@ -0,0 +1,25 @@
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { DisplayProductQuery } from "../display-product.query";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { Product } from "src/products/entities/product.entity";
+import { NotFoundException } from "@nestjs/common";
+
+
+@QueryHandler(DisplayProductQuery)
+export class DisplayProductHandler implements IQueryHandler<DisplayProductQuery> {
+    constructor(
+    @InjectRepository(Product)
+    private productRespository: Repository<Product>) {
+    }
+
+    async execute(query: DisplayProductQuery): Promise<any> {
+        console.log('******************* asyn display product')
+        const product = await this.productRespository.findOne({ where: { id } });
+        if (!product || product.isDeleted) {
+          throw new NotFoundException('Product not found');
+        }
+        return product;
+    }
+}
