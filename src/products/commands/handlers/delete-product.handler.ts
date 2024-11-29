a55590592d20759088aa17b8d45165d28df34cb4
kimiavedad <kimia.ved@gmail.com>
2024-11-29 18:12:21 +0330

    Add delete product command and handler

---
+++ src/products/commands/handlers/delete-product.handler.ts
@@ -0,0 +1,26 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { DeleteProductCommand } from '../delete-product.command';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+
+@CommandHandler(DeleteProductCommand)
+export class DeleteProductHandler
+  implements ICommandHandler<DeleteProductCommand>
+{
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  async execute(command: DeleteProductCommand): Promise<any> {
+    const { id } = command;
+    const product = await this.productRepository.findOne({ where: { id } });
+    if (!product || product.isDeleted) {
+      throw new NotFoundException('Product not found.');
+    }
+    await this.productRepository.softDelete(id);
+    // TODO: record history
+  }
+}
